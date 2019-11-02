7916babd69b1d43a0daeee1612b959edd6352381
VelychkoOleksandr <oleksandr.velychko.mail@gmail.com>
2019-11-02 04:01:28 +0200

    Header connected to Redux store

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 import './header.css';
 
-const ShopHeader = ({ numItems = 4, total }) => {
+const ShopHeader = ({ numOfItems, total = 0 }) => {
     return(
        <header className="shop-header row">
            <Link className="logo text-dark" to="/">ReStore</Link>
            <Link className="shopping-cart" to="/cart/">
                <i className="cart-icon fa fa-shopping-cart" />
-               { numItems } items (${total})
+               { numOfItems } items (${total})
            </Link>
        </header> 
     );
 }
 
-export default ShopHeader;
+const mapStatetoProps = ({ shoppingCart: { cartItems, orderTotal }}) => ({
+    numOfItems: cartItems.length,
+    total: orderTotal
+  });
+
+export default connect(mapStatetoProps)(ShopHeader);
